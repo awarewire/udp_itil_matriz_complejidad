93e958e9583f24051e4d585557f0bebd634acb8e
Jose Valladares <jose.valladares@externos-cl.cencosud.com>
2022-05-01 02:42:25 -0400

    Initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatrizComplejidadComponent } from './pages/matriz-complejidad/matriz-complejidad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatrizComplejidadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/matriz-complejidad/matriz-complejidad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matriz-complejidad',
+  templateUrl: './matriz-complejidad.component.html',
+  styleUrls: ['./matriz-complejidad.component.css']
+})
+export class MatrizComplejidadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
